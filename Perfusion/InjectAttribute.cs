dd8bd35aff9b1c69972258759a691c1384f7ab60
Nikolay Kochulin <porez0xfeedface@gmail.com>
2018-12-18 16:20:27 +0300

    Allow injectattribute on parameters

--- Perfusion/InjectAttribute.cs
+++ Perfusion/InjectAttribute.cs
@@ -1,6 +1,6 @@
 namespace Perfusion
 {
-    [System.AttributeUsage(System.AttributeTargets.Field | System.AttributeTargets.Method | System.AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
+    [System.AttributeUsage(System.AttributeTargets.Field | System.AttributeTargets.Method | System.AttributeTargets.Property | System.AttributeTargets.Parameter, Inherited = true, AllowMultiple = false)]
     public sealed class InjectAttribute : System.Attribute
     {
         public InjectAttribute() { }
